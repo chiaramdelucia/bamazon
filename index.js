74f9b71417c7c12ba21cd31bb70ea5b67386d73b
Chiara DeLucia <chiaramdelucia@gmail.com>
2017-02-27 22:21:02 -0500

    Initial functions for entering store.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var mysql = require('mysql');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
 
-var connection = mysql.createConnection ({
+const connection = mysql.createConnection ({
 	host: 'localhost',
 	port: 3306,
 	user: 'root',
@@ -14,3 +15,47 @@ connection.connect (function (err){
 	console.log('connected as id ' + connection.threadID);
 
 });
+
+
+function showProducts() {
+	connection.query('SELECT item_id, product_name, price FROM products', function (err, results) {
+		if (err) throw err;
+		console.log("Welcome to Bamazon! Please view our cataloge and type BUY to make a purchase.");
+		console.log(results);
+	});
+
+	function enterApp() {
+	inquirer.prompt([
+
+		{
+			type: 'list',
+			message: "Welcome to Bamazon! Please view our cataloge + choose BUY to make a purchase.",
+			choices: ['BUY',
+						'GTFO'],
+			name: 'enter'
+		}
+
+
+	]).then(function(user){
+
+
+
+		switch (user.enter){
+		
+			case 'BUY': 
+				return console.log('hello!');
+				break;
+
+			case 'GTFO':
+			return console.log('see ya later');
+			break;
+			};
+
+			
+		});
+
+	};
+	enterApp();
+};
+
+showProducts();
